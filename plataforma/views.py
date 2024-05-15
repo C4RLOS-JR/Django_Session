addeeb075ad2f89f96dadfc903ec13d8b97c3b4a
Leonam-Comercial <leonam.comseg@gmail.com>
2024-05-15 13:57:59 -0300

    Leonam

--- plataforma/views.py
+++ plataforma/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import redirect, render
 from django.http import HttpResponse
 
 def home(request):
-  if request.session['logado']:
-    return render(request, 'home.html')
+  if request.session.get('logado'): # tenta pegar a sessionid do usuário.
+    return render(request, 'home.html', {'logado': request.session['logado']})
   else:
     return redirect('/auth/login/?status=7')
