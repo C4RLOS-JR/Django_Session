b2a8cf4c3ae9518fa2498bc027a498d985c3cf41
C4RLOS-JR <camjr3z@gmail.com>
2024-05-19 18:57:28 -0300

    Carlos

--- plataforma/views.py
+++ plataforma/views.py
@@ -4,8 +4,8 @@ from django.contrib import messages
 from django.contrib.messages import constants
 
 def home(request):
-  if request.session.get('logado'): # tenta pegar a sessionid do usuário.
-    return render(request, 'home.html', {'logado': request.session['logado']})
-  else:
-    messages.add_message(request, constants.WARNING, 'Você precisa estar logado para acessar a plataforma!')
-    return redirect('/auth/login/')
+  # if request.session.get('logado'): # tenta pegar a sessionid do usuário.
+  #   return render(request, 'home.html', {'logado': request.session['logado']})
+  # else:
+    # messages.add_message(request, constants.WARNING, 'Você precisa estar logado para acessar a plataforma!')
+  return redirect('/auth/login/')
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,10 +1,10 @@
-from types import new_class
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from hashlib import sha256
 from .models import Usuario
 from django.contrib.messages import constants
-from django.contrib import messages
+from django.contrib import messages, auth
+from django.contrib.auth.models import User
 
 def cadastro(request):
   status = request.GET.get('status')
@@ -14,7 +14,7 @@ def valida_cadastro(request):
   nome = request.POST.get('nome')
   email = request.POST.get('email')
   senha = request.POST.get('senha')
-  usuario_existe = Usuario.objects.filter(email=email)
+  # usuario_existe = Usuario.objects.filter(email=email)
 
   if len(nome.strip()) == 0 or len(email.strip()) == 0:
     messages.add_message(request, constants.WARNING, 'Os campos "Nome" e "Email" não podem ficar em branco!')
@@ -22,17 +22,21 @@ def valida_cadastro(request):
   if len(senha.strip()) < 6:
     messages.add_message(request, constants.WARNING, 'A senha tem que ter no mínimo 6 caracteres!')
     return redirect('/auth/cadastro/')
-  if usuario_existe:
+  # if usuario_existe:
+  #   messages.add_message(request, constants.WARNING, 'Já existe um usuário cadastrado com esse Email!')
+  #   return redirect('/auth/cadastro/')
+
+  if User.objects.filter(email = email):
     messages.add_message(request, constants.WARNING, 'Já existe um usuário cadastrado com esse Email!')
     return redirect('/auth/cadastro/')
     
   try:
-    senha = sha256(senha.encode()).hexdigest()
-    novo_usuario = Usuario(nome = nome,
-                           email = email,
-                           senha = senha)
+    # senha = sha256(senha.encode()).hexdigest()
+    novo_usuario = User.objects.create_user(username = nome,
+                                            email = email,
+                                            password = senha)
     novo_usuario.save()
-    messages.add(request, constants.SUCCESS, 'Usuário cadastrado com sucesso!')
+    messages.add_message(request, constants.SUCCESS, 'Usuário cadastrado com sucesso!')
     return redirect('/auth/cadastro/')
   except:
     messages.add_message(request, constants.ERROR, 'Erro interno do sistema...tente novamente!')
@@ -43,25 +47,23 @@ def login(request):
   return render(request, 'login.html', {'status': status})
 
 def valida_login(request):
-  email = request.POST.get('email')
+  nome = request.POST.get('nome')
   senha = request.POST.get('senha')
-  senha = sha256(senha.encode()).hexdigest()
-  usuario_existe = Usuario.objects.filter(email=email)
+  # senha = sha256(senha.encode()).hexdigest()
+  usuario_existe = auth.authenticate(request, username = nome, password = senha)
 
   if not usuario_existe:
-    messages.add_message(request, constants.ERROR, 'Email não cadastrado no sistema!')
-    return redirect('/auth/login/')
-  if usuario_existe[0].senha != senha:
-    messages.add_message(request, constants.ERROR, 'Senha inválida!')
-    return redirect('/auth/login/')
+    messages.add_message(request, constants.ERROR, 'Usuário não cadastrado no sistema!')
+    return redirect('/auth/login/')  
   
-  
-  request.session['logado'] = True  # mostra se o usuário está logado.
-  request.session['usuario_id'] = usuario_existe[0].id  # mostra o ID do usuário que está logado.
-  messages.add_message(request, constants.SUCCESS, f'Olá {usuario_existe[0].nome}...Seja bem vindo!')
-  return redirect('/plataforma/home')
+  # request.session['logado'] = True  # mostra se o usuário está logado.
+  # request.session['usuario_id'] = usuario_existe[0].id  # mostra o ID do usuário que está logado.
+
+  auth.login(request, usuario_existe)
+  messages.add_message(request, constants.SUCCESS, f'Olá {usuario_existe.username}...Seja bem vindo(a)!')
+  return render(request, 'home.html')
 
 def sair(request):
   #return HttpResponse(request.session.get_expiry_date())
-  request.session.flush()  # deleta a session por completo.
+  # request.session.flush()  # deleta a session por completo.
   return redirect('/auth/login')
