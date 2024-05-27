c3c5c31984b8cf22c3e1415c2f5756f2c201fcbd
C4RLOS-JR <camjr3z@gmail.com>
2024-05-27 19:22:13 -0300

    Carlos

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth import admin as admin_auth_django
 from .models import Users
+from .forms import UserChangeForm, UserCreationForm
 
-admin.site.register(Users, UserAdmin)
