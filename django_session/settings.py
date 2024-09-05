19e42ba290e6aa18dab2353935288f2cbd5c2d74
C4RLOS-JR <camjr3z@gmail.com>
2024-09-05 19:44:20 -0300

    Carlos

--- django_session/settings.py
+++ django_session/settings.py
@@ -104,7 +104,12 @@ SESSION_COOKIE_AGE = 60 * 60 * 24
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'),)
+STATIS_ROOT = os.path.join('static')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- plataforma/forms.py
+++ plataforma/forms.py
@@ -3,3 +3,11 @@ from django import forms
 class Cliente(forms.Form):
   nome = forms.CharField(max_length=30, required=False)
   idade = forms.IntegerField(required=False)
+
+  def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+    self.fields['nome'].widget.attrs['class'] = 'form-control'
+    self.fields['nome'].widget.attrs['placeholder'] = 'Digite seu nome'
+    
+    self.fields['idade'].widget.attrs['class'] = 'form-control'
+    self.fields['idade'].widget.attrs['placeholder'] = 'Digite sua idade'
