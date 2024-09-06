eea2c52ab9b8c76ef5d54c20827a21a656d83b5f
Leonam-Comercial <leonam.comseg@gmail.com>
2024-09-06 16:25:53 -0300

    Leonam

--- plataforma/forms.py
+++ plataforma/forms.py
@@ -1,8 +1,11 @@
 from django import forms
+from .models import Livros
 
 class Cliente(forms.Form):
   nome = forms.CharField(max_length=30, required=False)
   idade = forms.IntegerField(required=False)
+  data = forms.DateField()
+  email = forms.EmailField()
 
   def __init__(self, *args, **kwargs):
     super().__init__(*args, **kwargs)
@@ -11,3 +14,15 @@ class Cliente(forms.Form):
     
     self.fields['idade'].widget.attrs['class'] = 'form-control'
     self.fields['idade'].widget.attrs['placeholder'] = 'Digite sua idade'
+    
+    self.fields['data'].widget.attrs['class'] = 'form-control'
+    self.fields['data'].widget.attrs['placeholder'] = 'Digite a data'
+    
+    self.fields['email'].widget.attrs['class'] = 'form-control'
+    self.fields['email'].widget.attrs['placeholder'] = 'Digite seu email'
+
+
+class Livro(forms.ModelForm):
+  class Meta:
+    model = Livros
+    fields = '__all__'
--- plataforma/views.py
+++ plataforma/views.py
@@ -4,9 +4,34 @@ from django.contrib import messages
 from django.contrib.messages import constants
 from django.contrib.auth.decorators import login_required
 from .forms import Cliente
+from .forms import Livro
 
 
 @login_required(login_url='/auth/login')
 def home(request):
-  form = Cliente()
-  return render(request, 'home.html', {'form': form})
+  # if request.method == 'GET':
+  #   form = Cliente()
+  #   return render(request, 'home.html', {'form': form})
+  # elif request.method == 'POST':
+  #   form = Cliente(request.POST)
+  #   if form.is_valid():
+  #     nome = form.data['nome']
+  #     idade = form.data['idade']
+  #     data = form.data['data']
+  #     email = form.data['email']
+  #     form.cleaned_data
+
+  #     messages.add_message(request, constants.SUCCESS, 'Formulário enviado com sucesso!')
+  #     return redirect('home')
+
+  #   return render(request, 'home.html', {'form': form})
+
+  if request.method == 'GET':
+    form = Livro()
+    return render(request, 'home.html', {'form': form})
+  elif request.method == 'POST':
+    form = Livro(request.POST)
+    if form.is_valid():
+      form.save()
+      form.cleaned_data
+    return render(request, 'home.html', {'form': form})
